ab80471527e0b6deab99858667e37c93272afcdf
smilewendy7 <smilewendy7@gmail.com>
2021-09-07 18:42:15 -0400

    setting up

--- WENDY/settings.py
+++ WENDY/settings.py
@@ -93,12 +93,7 @@ WSGI_APPLICATION = 'WENDY.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+DATABASES = {'default': env.db()}
 
 
 # Password validation
